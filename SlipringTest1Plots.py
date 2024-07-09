ddf0b909de18f8e2f4d709576bdcf6ef156f469b
Laurel O'Brien <laurelobrien126@gmail.com>
2024-07-09 09:40:30 -0600

    added 2nd and 3rd test

--- SlipringTest1Plots.py
+++ SlipringTest1Plots.py
@@ -14,17 +14,18 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     
-    dataFile = pd.read_csv('/Users/bloss/OneDrive/documents/DataSets/ResistanceData-2024-7-03_SlipringTest1.csv')
+    dataFile = pd.read_csv('/Users/bloss/OneDrive/documents/DataSets/ResistanceData-2024-7-08_SlipringTest3.csv')
 
     dataTime = dataFile.get("Time")/1000/1000/60
 
     plt.plot(dataTime, dataFile.get(" Res 2"))
     plt.plot(dataTime, dataFile.get(" Res 4"))
     plt.plot(dataTime, dataFile.get(" Res 6"))
-    plt.title("Slipring Test 1 (-20C)")
+    plt.title("Slipring Test 3 (-20C)")
     plt.xlabel("Time [min]")
     plt.ylabel("Resistance [ohms]")
     plt.legend(["Res 1","Res 2","Res 3"])
-    plt.text(45, 90, 'Not rotating last 20 min', fontsize = 10)
+    plt.plot([80], linestyle = 'dashed')
+    #plt.text(45, 90, 'Not rotating last 20 min', fontsize = 10)
     plt.grid()
     plt.show()
